73d53fa6b12b075d3079dc88c31a525a0b8ac882
Bendegúz Cseresnyés <cser.bende@gmail.com>
2022-05-07 01:59:33 +0200

    Add new app to index

--- workspaces/frontend/src/app/index.tsx
+++ workspaces/frontend/src/app/index.tsx
@@ -3,15 +3,25 @@ import { BrowserRouter, Routes } from "react-router-dom";
 import { appRoutes } from "./appRoutes";
 import Layout from "./Layout";
 import ChakraProvider from "./ChakraProvider";
+import { Suspense } from "react";
+import { Center, Spinner } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>{appRoutes}</Routes>
-        </Layout>
-      </BrowserRouter>
+      <Suspense
+        fallback={
+          <Center height="100vh">
+            <Spinner size="xl" />
+          </Center>
+        }
+      >
+        <BrowserRouter>
+          <Layout>
+            <Routes>{appRoutes}</Routes>
+          </Layout>
+        </BrowserRouter>
+      </Suspense>
     </ChakraProvider>
   );
 }
