179542168326877ba17ff5c6656f37b408ceb04b
Attila Makai <sandorattilamakai@gmail.com>
2022-05-07 23:25:38 +0200

    added missing emptyfile component

---
+++ workspaces/frontend/src/components/EmptyPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Center, VStack } from "@chakra-ui/react";
+import { MdElectricScooter } from "react-icons/md";
+
+type Props = {
+  text: string;
+};
+
+export default function EmptyPage({ text }: Props) {
+  return (
+    <Center height="100%" width="100%">
+      <VStack>
+        <MdElectricScooter size="3xl" />
+        <Box>{text}</Box>
+      </VStack>
+    </Center>
+  );
+}
