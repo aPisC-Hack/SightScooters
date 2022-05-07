af41a0b7b24e5d83882e04f26278d15090c9c83b
Bendegúz Cseresnyés <cser.bende@gmail.com>
2022-05-07 07:46:55 +0200

    WIP

--- workspaces/frontend/src/queries/tour.query.ts
+++ workspaces/frontend/src/queries/tour.query.ts
@@ -1,16 +1,24 @@
 import { ITour } from "../../../common/src/ITour";
 
 import axios from "axios";
+import { LocalStorageQuery } from "./localstorage.query";
 
 async function getOne(id: string | undefined): Promise<ITour | undefined> {
   if (!id) return undefined;
-  return axios
-    .get(`https://crafthack.apisc.host/api/tour/${id}`)
-    .then((r) => r.data);
+
+  return (
+    LocalStorageQuery.getEntity("customTours", id) ||
+    axios.get(`https://crafthack.apisc.host/api/tour/${id}`).then((r) => r.data)
+  );
 }
 
 async function getNear(): Promise<ITour[]> {
+  const custom = LocalStorageQuery.get("customTours");
   return axios.get(`https://crafthack.apisc.host/api/tour`).then((r) => r.data);
+}
+
+async function uploadCustomTour(tour: ITour) {
+  return tour;
 }
 
 export const TourQuery = { getOne, getNear };
