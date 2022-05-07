88e18dfe5ad797046949b6de3dd08554a2f01c16
Bendegúz Cseresnyés <cser.bende@gmail.com>
2022-05-07 17:13:28 +0200

    Add landmark nav Modal

--- workspaces/frontend/src/pages/NavigationPage/LandmarkNavigationPanel.tsx
+++ workspaces/frontend/src/pages/NavigationPage/LandmarkNavigationPanel.tsx
@@ -1,51 +1,56 @@
 import { ChevronDownIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import {
   Box,
+  Button,
   Center,
   HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Text,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
 import { ILandmark } from "../../../../common/src/ILandmark";
+import LandmarkHeader from "../../components/landmark/LandmarkHeader";
+import LandmarkPictureBox from "../../components/landmark/LandmarkPictureBox";
 
 type Props = { landmark: ILandmark };
 
 export default function LandmarkNavigationPanel({ landmark }: Props) {
-  const { isOpen, onToggle } = useDisclosure();
+  const { isOpen, onToggle, onClose } = useDisclosure();
 
   return (
-    <Box
-      pos="absolute"
-      top={0}
-      left={0}
-      zIndex={1600}
-      right={0}
-      bottom={isOpen ? 0 : undefined}
-    >
-      <VStack
-        width="100%"
-        justify="center"
-        h={isOpen ? "100%" : undefined}
-        zIndex={10}
-        spacing={0}
+    <Box pos="absolute" top={0} left={0} bottom={isOpen ? 0 : undefined}>
+      <HStack
+        p={2}
+        borderBottomRadius={4}
+        w={isOpen ? "100%" : undefined}
+        bg="white"
       >
-        <HStack align="flex-start" w="100%">
-          <HStack
-            p={2}
-            borderBottomRadius={4}
-            w={isOpen ? "100%" : undefined}
-            bg="white"
-          >
-            <Text fontSize="lg" fontWeight="bold" onClick={onToggle}>
-              {landmark.name}
-              {!isOpen ? <ChevronRightIcon /> : null}
+        <Text fontSize="lg" fontWeight="bold" onClick={onToggle}>
+          {landmark.name}
+          <ChevronRightIcon />
+        </Text>
+      </HStack>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxW="90vw" mt={4}>
+          <ModalHeader>{landmark.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody height="80vh">
+            <LandmarkPictureBox images={landmark.pictures} height="60vmin" />
+            <Text mt={2} textAlign="justify">
+              {landmark.description}
             </Text>
-          </HStack>
-        </HStack>
-        <Box flex={1} bg="white" w="100%" h={isOpen ? undefined : "100%"}></Box>
-      </VStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
