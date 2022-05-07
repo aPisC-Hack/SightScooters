21a2bc2d8906d1e2351297ee924dcc2df114fbc0
Bendegúz Cseresnyés <cser.bende@gmail.com>
2022-05-07 07:47:48 +0200

    WIP

---
+++ workspaces/frontend/src/queries/localstorage.query.ts
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from "uuid";
+
+async function set(key: string, value: any) {
+  localStorage.setItem(key, value);
+}
+
+async function get(key: string): Promise<any> {
+  return localStorage.getItem(key);
+}
+
+async function insertEntity(key: string, entity: any) {
+  const list = (await get(key)) || [];
+  entity = {
+    ...entity,
+    id: uuidv4(),
+  };
+
+  set(key, [...list, entity]);
+}
+
+async function deleteEntity(key: string, id: string) {
+  const list: Array<any> = (await get(key)) || [];
+  set(
+    key,
+    list.filter((x) => x.id !== id)
+  );
+}
+
+async function getEntity(key: string, id: string) {
+  const list: Array<any> = (await get(key)) || [];
+  return list.find((x) => x.id === id);
+}
+
+export const LocalStorageQuery = {
+  set,
+  get,
+  insertEntity,
+  deleteEntity,
+  getEntity,
+};
