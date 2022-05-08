69c4cbe3e06bbe5198130c10ac279e5c42aedc66
Bendegúz Cseresnyés <cser.bende@gmail.com>
2022-05-08 08:31:42 +0200

    Add api error handling

--- workspaces/frontend/src/hooks/useApiCall.ts
+++ workspaces/frontend/src/hooks/useApiCall.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 
 interface IUseApiCall<TResult> {
@@ -15,18 +16,25 @@ export function useApiCall<TResult>(
   const [value, setValue] = useState<TResult | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
+  const toast = useToast();
 
-  
-  
   const execute = useCallback(async () => {
     try {
       setIsLoading(true);
       const value = await callback();
+      if (value == null) throw new Error("Entrry not found");
       setValue(value);
       setError(null);
     } catch (err: any) {
       setError(err);
       setValue(null);
+      toast({
+        title: "Api error",
+        description: "Someting went wrong, please retry",
+        status: "error",
+        duration: 30000,
+        isClosable: true,
+      });
     }
     setIsLoading(false);
   }, [callback]);
@@ -34,16 +42,16 @@ export function useApiCall<TResult>(
   const update = useCallback((value) => {
     setValue(value);
   }, []);
-  
+
   // eslint-disable-next-line
-  useEffect(() => void(execute()), []);
-  
+  useEffect(() => void execute(), []);
+
   return {
     value,
     isLoading,
     error,
 
     execute,
-    update
+    update,
   };
 }
